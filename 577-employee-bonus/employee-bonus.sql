a44612d4757919d6ba9d1185493d4e7e9f91c905
Janhvi Singh <janhvisingh300@gmail.com>
2025-11-10 13:41:47 +0530

    Time: 875 ms (69.48%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 # Write your MySQL query statement below
 SELECT e.name, 
-       b.bonus 
-FROM Employee e
+b.bonus
+FROM Employee e 
 LEFT JOIN Bonus b
-ON e.empId   = b.empId  
+ON e.empId = b.empId
 WHERE b.bonus < 1000 OR b.bonus IS NULL;
